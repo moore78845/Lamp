dfdb516709f3336e2648f4d5049111e14cd3f476
moore78845 <103225496+moore78845@users.noreply.github.com>
2023-03-24 08:58:58 +1300

    Add files via upload
    can change colour and have a click box for the stem

---
+++ ClickLamps.java
@@ -0,0 +1,23 @@
+import ecs100.*;
+import java.awt.Color;
+/**
+ * Write a description of class ClickLamps here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ClickLamps
+{
+   Lamp lamp1 = new Lamp(50, 50, 50, Color.red);
+
+    /**
+     * Constructor for objects of class ClickLamps
+     */
+    public ClickLamps()
+    {
+     lamp1.draw();
+
+    }
+
+
+}
